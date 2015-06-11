d5ae1202cf027ff8a528597bb17b804deb4753a7
vinaySimility <vinay@simility.com>
2015-06-12 02:15:17 +0530

    regexes.yaml is moved to resources directory

--- src/main/java/ua_parser/Parser.java
+++ src/main/java/ua_parser/Parser.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2012 Twitter, Inc
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,13 +16,15 @@
 
 package ua_parser;
 
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
-
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.SafeConstructor;
 
 /**
  * Java implementation of <a href="https://github.com/tobie/ua-parser">UA Parser</a>
@@ -31,13 +33,21 @@ import org.yaml.snakeyaml.constructor.SafeConstructor;
  */
 public class Parser {
 
-  private static final String REGEX_YAML_PATH = "/ua_parser/regexes.yaml";
+  private static final String REGEX_YAML_PATH = "regexes.yaml";
+  private static File file;
+
+  static {
+    ClassLoader classLoader = Parser.class.getClassLoader();
+    file = new File(classLoader.getResource(REGEX_YAML_PATH).getFile());
+  }
+
   private UserAgentParser uaParser;
   private OSParser osParser;
   private DeviceParser deviceParser;
 
   public Parser() throws IOException {
-    this(Parser.class.getResourceAsStream(REGEX_YAML_PATH));
+
+    this(new FileInputStream(file));
   }
 
   public Parser(InputStream regexYaml) {
@@ -66,21 +76,22 @@ public class Parser {
   private void initialize(InputStream regexYaml) {
     Yaml yaml = new Yaml(new SafeConstructor());
     @SuppressWarnings("unchecked")
-    Map<String,List<Map<String,String>>> regexConfig = (Map<String,List<Map<String,String>>>) yaml.load(regexYaml);
+    Map<String, List<Map<String, String>>> regexConfig = (Map<String, List<Map<String, String>>>)
+            yaml.load(regexYaml);
 
-    List<Map<String,String>> uaParserConfigs = regexConfig.get("user_agent_parsers");
+    List<Map<String, String>> uaParserConfigs = regexConfig.get("user_agent_parsers");
     if (uaParserConfigs == null) {
       throw new IllegalArgumentException("user_agent_parsers is missing from yaml");
     }
     uaParser = UserAgentParser.fromList(uaParserConfigs);
 
-    List<Map<String,String>> osParserConfigs = regexConfig.get("os_parsers");
+    List<Map<String, String>> osParserConfigs = regexConfig.get("os_parsers");
     if (osParserConfigs == null) {
       throw new IllegalArgumentException("os_parsers is missing from yaml");
     }
     osParser = OSParser.fromList(osParserConfigs);
 
-    List<Map<String,String>> deviceParserConfigs = regexConfig.get("device_parsers");
+    List<Map<String, String>> deviceParserConfigs = regexConfig.get("device_parsers");
     if (deviceParserConfigs == null) {
       throw new IllegalArgumentException("device_parsers is missing from yaml");
     }
